d8df81962a513b48606b247795936682282c381f
avi8779 <avi87793919@gmail.com>
2024-01-03 17:51:53 +0530

    Added to Toaster React hot

--- src/main.jsx
+++ src/main.jsx
@@ -5,10 +5,12 @@ import './index.css'
 // Library imports
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { Toaster } from 'react-hot-toast';
 import { BrowserRouter } from 'react-router-dom'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
  <BrowserRouter>
     <App />
+    <Toaster />
   </BrowserRouter>
 )
