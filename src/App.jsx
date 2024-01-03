65ff72e7d405a1a05f1052ef8bb73857f3049a4a
avi8779 <avi87793919@gmail.com>
2024-01-03 17:47:00 +0530

    confiures auto import sorting for eslint

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
 
+import { Routes } from 'react-router-dom'
 import './App.css'
 
 function App() {
 
   return (
     <>
-     hello
+     <Routes>
+      {/* <Route path="/" element={<Home />}> </Route> */}
+     </Routes>
     </>
   )
 }
