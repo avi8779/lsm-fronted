2ab57700676c5bc9d73caec3f51dd91c7f2605b5
avi8779 <avi87793919@gmail.com>
2024-02-22 18:55:24 +0530

    Create a About page and create login logout button

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,14 @@ import { Routes, Route } from 'react-router-dom'
 import './App.css'
 import Footer from './Components/Footer'
 import HomePage from './Pages/HomePage'
+import AboutUs from './Pages/AboutUs'
 function App() {
 
   return (
     <>
-      <Routes> */
+      <Routes> 
         <Route path="/" element={<HomePage />}> </Route>
+        <Route path="/about" element={<AboutUs />}> </Route>
       </Routes>
     </>
   )
--- src/Layouts/HomeLayout.jsx
+++ src/Layouts/HomeLayout.jsx
@@ -1,10 +1,20 @@
 import { FiMenu } from "react-icons/fi";
 import { AiOutlineClose } from "react-icons/ai";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
+import { useDispatch, useSelector } from 'react-redux';
 import Footer from "../Components/Footer";
 
 
 function HomeLayout({ children }){
+
+    const dispatch = useDispatch();
+    const navigate = useNavigate();
+
+    // for checking if user is logged in
+    const isLoggedIn = useSelector((state) => state?.auth?.isLoggedIn);
+
+    // for displaying the options acc to role
+    const role = useSelector((state) => state?.auth?.role);
 
     function changeWidth() {
         const drawerSide = document.getElementsByClassName("drawer-side");
@@ -18,6 +28,14 @@ function hideDrawer() {
     const drawerSide = document.getElementsByClassName("drawer-side");
     drawerSide[0].style.width = "0";
 }
+
+async function handleLogout(e) {
+    e.preventDefault();
+
+    const res = await dispatch(logout());
+    if(res?.payload?.success)
+    navigate("/");
+    }
 
 
 
@@ -38,7 +56,7 @@ function hideDrawer() {
                     <label htmlFor="my-drawer" className="drawer-overlay">
 
                     </label>
-                    <ul className="menu p-4 w-48 sm:w-80 bg-base-100 text-base-content relative">
+                    <ul className="menu p-4 w-48 h-[100%] sm:w-80 bg-base-100 text-base-content relative">
                         <li className="w-fit absolute right-2 z-50">
                             <button
                             onClick={hideDrawer}>
@@ -49,6 +67,12 @@ function hideDrawer() {
                         <li>
                             <Link to="/">Home</Link>
                         </li>
+
+                        {isLoggedIn && role === 'ADMIN' && (
+                            <li>
+                                <Link to="/admin/dashboard">Admin DashBoard</Link>
+                            </li>
+                        )}
 
                         <li>
                             <Link to="/courses">All Courses</Link>
@@ -61,6 +85,36 @@ function hideDrawer() {
                         <li>
                             <Link to="/about">About Us</Link>
                         </li>
+
+                        {!isLoggedIn && (
+                            <li className="absolute bottom-4 w-[90%]">
+                            <div className="w-full h-[100%] flex items-center justify-center">
+                                <button className=" btn-primary px-4 py-1 font-semibold rounded-md w-full bg-white">
+                                    <Link to="/login">Login</Link>
+                                </button>
+
+                                <button className=" btn-secondary px-4 py-1 font-semibold rounded-md w-full">
+                                    <Link to="/login">Sign Up</Link>
+                                </button>
+                            </div>
+                            </li>
+                         )}
+
+                         {isLoggedIn && (
+                            <li className="absolute bottom-4 w-[90%]">
+                            <div className="w-full flex items-center justify-center">
+                                <button className=" btn-primary px-4 py-1 font-semibold rounded-md w-full bg-white">
+                                    <Link to="/user/profile">profile</Link>
+                                </button>
+
+                                <button className=" btn-secondary px-4 py-1 font-semibold rounded-md w-full">
+                                    <Link onClick={handleLogout}>Logout</Link>
+                                </button>
+                            </div>
+                            </li>
+                         )}
+
+                         
                     </ul>
                 </div>
             </div>
